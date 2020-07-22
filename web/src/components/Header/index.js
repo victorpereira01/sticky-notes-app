fc4b6198be5f4d597bbd830c91462b5389561bb8
Victor Pereira <victor.cordeiro2@gmail.com>
2020-07-21 22:24:59 -0300

    Web create + Header component

---
+++ web/src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles.css';
+
+const Header = () => (
+    <header id="main-header">Minha Agenda<button> + Evento</button></header>
+)
+    
+export default Header;
